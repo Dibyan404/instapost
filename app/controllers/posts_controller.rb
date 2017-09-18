9ae6737918b4f61dc23a9bc163c03217aa597975
Dibyan Maharjan <dibyan@acd.edu.np>
2017-09-18 06:38:53 +0000

    Setting up User Forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
     def create 
-    @post = Post.find(params[:id])
-    @post.create(post_params)
-    redirect_to root_path
+        @post = Post.find(params[:id])
+        @post.create(post_params)
+        redirect_to root_path
     end
+    def new
+        @post = Post.new
+    end
+    
 end
