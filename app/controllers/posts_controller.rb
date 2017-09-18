82feb5313af71a68634e3003d253b7dae37e08b5
Dibyan Maharjan <dibyan@acd.edu.np>
2017-09-18 07:22:05 +0000

    Writing the Post BackEnd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,28 @@
 class PostsController < ApplicationController
-    def create 
-        @post = Post.find(params[:id])
-        @post.create(post_params)
-        redirect_to root_path
-    end
+    before_action :authenticate_user!, only: [:new, :create]
+    
+    #def create
+     #   @post = Post.find(params[:id])
+      #  @post.create(post_params)
+       # redirect_to root_path
+    #end
+    
     def new
         @post = Post.new
     end
     
+    
+    def create
+        @post = current_user.posts.create(post_params)
+        #What this is saying is, create a post by current_user. This automatically hooks up and connects the current_user with the created post.
+        if(@post.valid?)
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    private
+    def post_params
+        params.require(:post).permit(:user_id, :photo, :description)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
     mount_uploader :photo, PhotoUploader
+    belongs_to :user
+    validates :photo, :dexcription, :user_id, presence: true
 end
